b2557388ef26b5e5c99f9f4e49abd38d5592681a
baslobodnik@gmail.com <brandi@brandi-VirtualBox.(none)>
2011-10-25 20:26:17 -0500

    First commit

---
+++ clubs/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from clubs.models import Location, College, Major, Advisor, Organization, Question
+
+admin.site.register(Location)
+admin.site.register(College)
+admin.site.register(Major)
+admin.site.register(Advisor)
+admin.site.register(Organization)
+admin.site.register(Question)
+admin.autodiscover()
---
+++ clubs/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    def __unicode__(self):
+        return self.location_name
+
+class College(models.Model):
+    name = models.CharField(max_length=255)
+    name_slug = models.SlugField()
+    def __unicode__(self):
+        return self.name
+
+class Major(models.Model):
+    name = models.CharField(max_length=255)
+    name_slug = models.SlugField()
+    def __unicode__(self):
+        return self.name
+
+class Advisor(models.Model):
+    name = models.CharField(max_length=255)
+    name_slug = models.SlugField()
+    title = models.CharField(max_length=250)
+    def __unicode__(self):
+        return self.name
+
+class Organization(models.Model):
+    club_title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField('date published')
+    pertaining_college = models.ManyToManyField(College)
+    pertaining_major = models.ManyToManyField(Major)
+    advisor = models.ManyToManyField(Advisor)
+    meeting_dates = models.TextField()
+    location = models.ForeignKey(Location)
+    application = models.BooleanField()
+    fee = models.BooleanField()
+    def was_published_today(self):
+        return self.pub_date.date() == datetime.date.today()
+
+class Question(models.Model):
+    organization = models.ForeignKey(Organization)
+    question = models.CharField(max_length=255)
+    answer = models.TextField(blank=True, null=True)
+    def __unicode__(self):
+        return self.question
